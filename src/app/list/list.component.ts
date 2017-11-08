74ce7c4c364af3c7c55dffd9b6c215de3d4bff47
larslemos <larslemos@gmail.com>
2017-11-08 20:53:44 +0200

    Add map feature

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from "../data.service";
 import { Coffe } from "../logic/Coffe";
 import { Router } from '@angular/router';
+import { GeolocationService } from '../geolocation.service';
 
 @Component({
   selector: 'app-list',
@@ -13,11 +14,19 @@ export class ListComponent implements OnInit {
   list: [Coffe]
 
   constructor(private data: DataService,
-              private router: Router) { }
+              private router: Router,
+              private geolocation: GeolocationService) { }
 
   goDetails(coffe: Coffe) {
         this.router.navigate(['/coffe', coffe._id]);
   }
+
+  goMap(coffe: Coffe) {
+    const mapURL = this.geolocation.getMapLink(coffe.location);
+    location.href = mapURL;
+
+  }
+
 
   ngOnInit() {
       this.data.getList(list => {
